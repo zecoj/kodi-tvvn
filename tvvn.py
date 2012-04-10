1f10c9dbec1cb5388618ecfb1c3a1453c3956dd7
Binh Nguyen <b@zecoj.com>
2012-04-10 10:03:41 +1000

    2012.04.10-10:03:41

--- tvvn.py
+++ tvvn.py
@@ -48,13 +48,14 @@ def get_params():
 		cleanedparams=params.replace('?','')
 		if (params[len(params)-1]=='/'):
 			params=params[0:len(params)-2]
-			pairsofparams=cleanedparams.split('&')
-			param={}
-			for i in range(len(pairsofparams)):
-				splitparams={}
-				splitparams=pairsofparams[i].split('=')
-				if (len(splitparams))==2:
-					param[splitparams[0]]=splitparams[1]
+		pairsofparams=cleanedparams.split('&')
+		param={}
+		for i in range(len(pairsofparams)):
+			splitparams={}
+			splitparams=pairsofparams[i].split('=')
+			if (len(splitparams))==2:
+				param[splitparams[0]]=splitparams[1]
+							
 	return param
 
 params=get_params()
